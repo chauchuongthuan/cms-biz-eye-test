cadfe5adb234b1b6fede10f39f513ce10c14a1ec
Diginext <thinhnguyen@wearetopgroup.com>
2023-09-12 19:00:57 +0700

    Update page

--- src/app/modules/pages/components/page-home/page-home.component.ts
+++ src/app/modules/pages/components/page-home/page-home.component.ts
@@ -1,49 +1,140 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { PageService } from "../../services/page.service";
+import { convertToFormDataV2 } from "src/app/shared/helper";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-page-home',
-  templateUrl: './page-home.component.html',
-  styleUrls: ['./page-home.component.scss']
+   selector: "app-page-home",
+   templateUrl: "./page-home.component.html",
+   styleUrls: ["./page-home.component.scss"],
 })
 export class PageHomeComponent implements OnInit {
-  
-  form: FormGroup;
-  public bannerImageMobile = [];
-  public bannerImage = [];
+   form: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+   public bannerImageMobile: any = [];
+   public bannerImage: any = [];
+   public bannerClient: any = [];
+   public submitted: boolean = false;
+   public id: any = "";
+   constructor(
+      private fb: FormBuilder,
+      private pageService: PageService,
+      private router: ActivatedRoute,
+   ) {
+      const id = router.snapshot.queryParamMap.get("id");
+      this.id = id;
+      this.getDetailPages();
+   }
 
-  ngOnInit() {
-    this.form = this.fb.group({
-      heroBanner: this.fb.array([])
-    });
-  }
+   ngOnInit() {
+      this.form = this.fb.group({
+         heroBanner: this.fb.array([]),
+         clients: this.fb.array([]),
+      });
+   }
 
-  get lines(): FormArray {
-    return this.form.get('heroBanner') as FormArray;
-  }
+   get lines(): FormArray {
+      return this.form.get("heroBanner") as FormArray;
+   }
 
-  addLine() {
-    this.lines.push(this.fb.group({
-      bannerImage: '',
-      bannerImageMobile: '',
-      link: '',
-      video: '',
-      children: this.fb.array([])
-    }));
-  }
+   get clients(): FormArray {
+      return this.form.get("clients") as FormArray;
+   }
 
-  removeLine(index: number) {
-    this.lines.removeAt(index);
-  }
+   // Call API get detail page fields to content
+   getDetailPages() {
+      this.pageService.getPageById(this.id).subscribe((data) => {
+         console.log(data);
+         // Push data in server hero banner
+         data?.content?.heroBanner?.forEach((item: { bannerImage: any; bannerImageMobile: any; link: any; video: any }) => {
+            if (item.bannerImage === "") {
+               item.bannerImage = null;
+            }
+            if (item.bannerImageMobile === "") {
+               item.bannerImageMobile = null;
+            }
+            this.lines.push(
+               this.fb.group({
+                  bannerImage: [{ value: "", preview: item.bannerImage }],
+                  bannerImageMobile: [{ value: "", preview: item.bannerImageMobile }],
+                  link: item.link,
+                  video: item.video,
+               }),
+            );
+            this.bannerImage.push(item.bannerImage);
+            this.bannerImageMobile.push(item.bannerImageMobile);
+         });
+         // Push data in server our clients
+         data?.content?.clients?.forEach((item: { bannerImage: any; link: any; order: any }) => {
+            this.clients.push(
+               this.fb.group({
+                  bannerImage: [{ value: "", preview: item.bannerImage }],
+                  link: item.link,
+                  order: item.order,
+               }),
+            );
+         });
 
-  changeFileUpload(data: any, index: number, field: string) {
-    this.lines.controls[index].get(field)?.setValue(data);
-  }
+         console.log(this.form.value);
+      });
+   }
 
-  submit() {
-    console.log(this.form.value);
-  }
+   // Add item in our clients in home page
+   addClient() {
+      this.clients.push(
+         this.fb.group({
+            bannerImage: new FormControl("", [Validators.required]),
+            link: "",
+            order: "",
+         }),
+      );
+   }
 
+   // Remove item in our clients in home page
+   removeClient(index: number) {
+      this.clients.removeAt(index);
+   }
+
+   // Add item in banner in home page
+   addLine() {
+      this.lines.push(
+         this.fb.group({
+            bannerImage: new FormControl({ value: "", preview: null }, [Validators.required]),
+            bannerImageMobile: new FormControl({ value: "", preview: null }, [Validators.required]),
+            link: "",
+            video: "",
+         }),
+      );
+   }
+
+   // Remove item in banner in home page
+   removeLine(index: number) {
+      this.lines.removeAt(index);
+   }
+
+   changeFileUpload(data: any, index: number, field: string, types: string) {
+      if (types === "lines") {
+         this.lines.controls[index].get(field)?.setValue([data]);
+      } else {
+         this.clients.controls[index].get(field)?.setValue([data]);
+      }
+   }
+
+   submit() {
+      this.submitted = true;
+      if (this.form.invalid) {
+         return;
+      }
+      console.log(this.form.value);
+      const data = {
+         content: this.form.value,
+      };
+      let formData = convertToFormDataV2(data, []);
+
+      this.pageService.editorPage(formData, this.id).subscribe((data) => {
+         console.log(data);
+      });
+   }
 }
+
--- src/app/modules/pages/pages/page.component.ts
+++ src/app/modules/pages/pages/page.component.ts
@@ -1,138 +1,143 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
-import { NzMessageService } from 'ng-zorro-antd/message';
-import { PageService } from '../services/page.service';
-import { Ipaginator, option, params, sort } from 'src/app/common/constant/list.model';
-import { HttpParams } from '@angular/common/http';
-import { RouterService } from 'src/app/common/services/router.service';
-import { CreateEditPageComponent } from '../components/create-edit-page/create-edit-page.component';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { FormControl, UntypedFormBuilder, UntypedFormGroup } from "@angular/forms";
+import { NzMessageService } from "ng-zorro-antd/message";
+import { PageService } from "../services/page.service";
+import { Ipaginator, option, params, sort } from "src/app/common/constant/list.model";
+import { HttpParams } from "@angular/common/http";
+import { RouterService } from "src/app/common/services/router.service";
+import { CreateEditPageComponent } from "../components/create-edit-page/create-edit-page.component";
+import { PageHomeComponent } from "../components/page-home/page-home.component";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-page',
-  templateUrl: './page.component.html',
-  styleUrls: ['./page.component.scss']
+   selector: "app-page",
+   templateUrl: "./page.component.html",
+   styleUrls: ["./page.component.scss"],
 })
 export class PageComponent implements OnInit {
-  public tableLoading: boolean = false;
-  public checked: boolean = true;
-  public expandSet = new Set<number>();
-  public filterForm!: UntypedFormGroup;
-  public listPage: any;
-  public visible = false;
-  public size: 'large' | 'default' = 'default';
-  public params: HttpParams = params;
-  public checkOptionsOne = [
-    { label: 'Tên danh mục', value: 'name', checked: true },
-    { label: 'Mô tả', value: 'shortDescription', checked: false },
-  ];
-  public paginator: Ipaginator = {
-    option: option,
-    limit: 10,
-    currentPage: 1,
-    total: 0,
-    pageCount: 0
-  };
-  public sort: sort = {
-    order: -1,
-    orderBy: 'createdAt'
-  }
-  // public listOfData = [];
-  @ViewChild('editForm') editForm: CreateEditPageComponent;
+   public tableLoading: boolean = false;
+   public checked: boolean = true;
+   public expandSet = new Set<number>();
+   public filterForm!: UntypedFormGroup;
+   public listPage: any;
+   public visible = false;
+   public size: "large" | "default" = "default";
+   public params: HttpParams = params;
+   public checkOptionsOne = [
+      { label: "Tên danh mục", value: "name", checked: true },
+      { label: "Mô tả", value: "shortDescription", checked: false },
+   ];
+   public paginator: Ipaginator = {
+      option: option,
+      limit: 10,
+      currentPage: 1,
+      total: 0,
+      pageCount: 0,
+   };
+   public sort: sort = {
+      order: -1,
+      orderBy: "createdAt",
+   };
+   // public listOfData = [];
+   @ViewChild("editForm") editForm: CreateEditPageComponent;
+   @ViewChild("pageHome") pageHome: PageHomeComponent;
 
-  constructor(
-    private fb: UntypedFormBuilder,
-    private pageService: PageService,
-    private msg: NzMessageService,
-    private routerService: RouterService
-  ) { }
+   constructor(
+      private fb: UntypedFormBuilder,
+      private pageService: PageService,
+      private msg: NzMessageService,
+      private routerService: RouterService,
+      private router: Router,
+   ) {}
 
-  ngOnInit(): void {
-    this.initParams();
-    this.filterForm = this.fb.group({
-      name: new FormControl('', []),
-      active: new FormControl('', []),
-      date: new FormControl('', []),
-    });
-    this.getPage(this.params);
-  }
+   ngOnInit(): void {
+      this.initParams();
+      this.filterForm = this.fb.group({
+         name: new FormControl("", []),
+         active: new FormControl("", []),
+         date: new FormControl("", []),
+      });
+      this.getPage(this.params);
+   }
 
-  initParams(){
-    this.params = this.params.set('orderBy', this.sort.orderBy)
-    this.params = this.params.set('order', this.sort.order)
+   initParams() {
+      this.params = this.params.set("orderBy", this.sort.orderBy);
+      this.params = this.params.set("order", this.sort.order);
 
-    let urlParams = this.routerService.params
-    let keys = Object.keys(urlParams)
-    keys.forEach((key, index) => {
-      this.params = this.params.set(key, urlParams[key])
-    })
-  }
-
-  onExpandChange(id: number, checked: boolean): void {
-    if (checked) {
-      this.expandSet.add(id);
-    } else {
-      this.expandSet.delete(id);
-    }
-  }
-
-  getPage(params: HttpParams) {
-    this.tableLoading = true;
-    this.pageService.getPage(params).subscribe((data: any) => {
-      this.listPage = data.list;
-      this.paginator = {
-        ...data.paginator,
-        option
-      }
-      this.tableLoading = false;
-      this.routerService.replaceParams(this.params);
-    })
-  }
-  onFilter(filters: any){    
-    if(filters){
-      let keys = Object.keys(filters)
-      let values = Object.values(filters)
+      let urlParams = this.routerService.params;
+      let keys = Object.keys(urlParams);
       keys.forEach((key, index) => {
-        if(!values[index]){
-          this.params = this.params.set(key, '')
-        }else if(['number', 'boolean', 'string'].includes(typeof values[index])){
-          this.params = this.params.set(key, `${values[index]}`)
-        }
-      })
-    }
-    this.getPage(this.params)
-  }
-  onEdit(data: any){
-    // this.editForm.visible = true;
-    this.editForm.initData(data);
-  }
+         this.params = this.params.set(key, urlParams[key]);
+      });
+   }
 
-  get title(): string {
-    return `${this.size} Drawer`;
-  }
+   onExpandChange(id: number, checked: boolean): void {
+      if (checked) {
+         this.expandSet.add(id);
+      } else {
+         this.expandSet.delete(id);
+      }
+   }
 
+   getPage(params: HttpParams) {
+      this.tableLoading = true;
+      this.pageService.getPage(params).subscribe((data: any) => {
+         this.listPage = data.list;
+         this.paginator = {
+            ...data.paginator,
+            option,
+         };
+         this.tableLoading = false;
+         this.routerService.replaceParams(this.params);
+      });
+   }
+   onFilter(filters: any) {
+      if (filters) {
+         let keys = Object.keys(filters);
+         let values = Object.values(filters);
+         keys.forEach((key, index) => {
+            if (!values[index]) {
+               this.params = this.params.set(key, "");
+            } else if (["number", "boolean", "string"].includes(typeof values[index])) {
+               this.params = this.params.set(key, `${values[index]}`);
+            }
+         });
+      }
+      this.getPage(this.params);
+   }
+   onEdit(data: any) {
+      // this.editForm.visible = true;
+      this.router.navigate([`admin/page-home`], { queryParams: { id: data.id } });
 
+      // this.pageHome.initData(data);
+   }
 
-  tableChange(e: any){
-    let arraySort = e.sort as Array<any>;
-    let fieldSort = arraySort.find((item) => item.value != null)
+   get title(): string {
+      return `${this.size} Drawer`;
+   }
 
-    let page = e.pageIndex;
-    let limit = e.pageSize;
-    let sort: sort = {
-      orderBy: fieldSort ? fieldSort.key : 'createdAt',
-      order: fieldSort?.value == 'ascend' ? 1 : -1
-    }
-    this.onChange(page, limit, sort)
-  }
+   tableChange(e: any) {
+      let arraySort = e.sort as Array<any>;
+      let fieldSort = arraySort.find((item) => item.value != null);
 
-  onChange(page: number, limit: number, sort: sort){
-    this.params = this.params.set('page', page)
-    this.params = this.params.set('limit', limit)
-    this.params = this.params.set('orderBy', sort.orderBy)
-    this.params = this.params.set('order', sort.order)
-    this.getPage(this.params)
-  }
-  onSuccess(){
-    this.getPage(this.params);
-  }
+      let page = e.pageIndex;
+      let limit = e.pageSize;
+      let sort: sort = {
+         orderBy: fieldSort ? fieldSort.key : "createdAt",
+         order: fieldSort?.value == "ascend" ? 1 : -1,
+      };
+      this.onChange(page, limit, sort);
+   }
+
+   onChange(page: number, limit: number, sort: sort) {
+      this.params = this.params.set("page", page);
+      this.params = this.params.set("limit", limit);
+      this.params = this.params.set("orderBy", sort.orderBy);
+      this.params = this.params.set("order", sort.order);
+      this.getPage(this.params);
+   }
+   onSuccess() {
+      this.getPage(this.params);
+   }
 }
+
