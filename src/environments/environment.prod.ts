ffaadca90a88598387d5d897b574abd37ba0b62d
chauchuongthuan <chauchuongthuan@gmail.com>
2023-10-16 11:37:51 +0700

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    BASE_URL: 'https://vgrtw7-bizeyes-apibizeyes.dev.diginext.site/api/v1/',
-    BASE_URL_GRAPHQL: 'https://vgrtw7-bizeyes-apibizeyes.dev.diginext.site/graphql',
-    BASE_URL_WS: 'wss://vgrtw7-bizeyes-apibizeyes.dev.diginext.site/graphql',
+    BASE_URL: 'https://testapibizeyes-testapibizeyes.dev.diginext.site/api/v1/',
+    BASE_URL_GRAPHQL: 'https://testapibizeyes-testapibizeyes.dev.diginext.site/graphql',
+    BASE_URL_WS: 'wss://testapibizeyes-testapibizeyes.dev.diginext.site/graphql',
   };
